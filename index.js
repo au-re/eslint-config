f99196d64750f08ccf6ff640c4789fa0a82e90f9
Aurélien <au.re@live.com>
2020-05-10 11:40:11 +0200

    relax airbnb rules

---
+++ index.js
@@ -0,0 +1,52 @@
+module.exports = {
+  // relaxes airbnb rules
+  "rules": {
+    "@typescript-eslint/quotes": [
+      "warn",
+      "double",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
+    "quotes": [
+      "warn",
+      "double",
+      {
+        "allowTemplateLiterals": true
+      }
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "arrow-body-style": "off",
+    "arrow-parens": [
+      "warn",
+      "always"
+    ],
+    "lines-between-class-members": "off",
+    "implicit-arrow-linebreak": "off",
+    "no-shadow": "off",
+    "padded-blocks": "off",
+    "object-curly-newline": "off",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": [
+          "rollup.config.js",
+          "**/stories/**/*.js",
+          "**/*test.ts",
+          "**/*test.tsx",
+          "**/*stories.tsx",
+          "src/setupTests.js",
+          "**/*__mocks__/**"
+        ]
+      }
+    ]
+  }
+};
