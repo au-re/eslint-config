af6d2dad671ba1e357e9c1d2ebe900f4d4a6da80
Aurélien <au.re@live.com>
2020-07-05 16:23:21 +0200

    ignore confusing arrow functions

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 module.exports = {
   // relaxes airbnb rules
-  "rules": {
+  rules: {
     "@typescript-eslint/quotes": [
       "warn",
       "double",
       {
-        "allowTemplateLiterals": true
-      }
+        allowTemplateLiterals: true,
+      },
     ],
-    "quotes": [
+    quotes: [
       "warn",
       "double",
       {
-        "allowTemplateLiterals": true
-      }
+        allowTemplateLiterals: true,
+      },
     ],
-    "comma-dangle": [
-      "error",
-      "always-multiline"
-    ],
+    "comma-dangle": ["error", "always-multiline"],
     "arrow-body-style": "off",
-    "arrow-parens": [
-      "warn",
-      "always"
-    ],
+    "arrow-parens": ["warn", "always"],
     "lines-between-class-members": "off",
     "implicit-arrow-linebreak": "off",
     "no-shadow": "off",
     "padded-blocks": "off",
     "object-curly-newline": "off",
     "class-methods-use-this": "off",
+    "no-confusing-arrow": "off",
     "no-param-reassign": "off",
     "no-underscore-dangle": "off",
     "import/prefer-default-export": "off",
@@ -37,16 +32,16 @@ module.exports = {
     "import/no-extraneous-dependencies": [
       "error",
       {
-        "devDependencies": [
+        devDependencies: [
           "rollup.config.js",
           "**/stories/**/*.js",
           "**/*test.ts",
           "**/*test.tsx",
           "**/*stories.tsx",
           "src/setupTests.js",
-          "**/*__mocks__/**"
-        ]
-      }
-    ]
-  }
+          "**/*__mocks__/**",
+        ],
+      },
+    ],
+  },
 };
